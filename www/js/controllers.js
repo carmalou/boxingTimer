7e646ec18369bfa5947334cf703281df9b506509
carmalou <carmenbourlonlong@gmail.com>
2016-05-26 11:39:04 -0500

    getting the timer started

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,53 +31,26 @@ angular.module('app.controllers', [])
   $scope.totalTimeRemaining = ((workoutFactory.workoutData.restTimeInMilliseconds * (workoutFactory.workoutData.rounds - 1)) + $scope.roundTimeRemaining);
 
   $scope.parseSeconds = function(totalMilliseconds) {
+    var timeObj = {};
     var seconds = totalMilliseconds / 1000;
     var minutes = seconds / 60;
     var hours = minutes / 60;
-
-    console.log('before modolus seconds', seconds);
-    console.log('before modolus minutes', minutes);
-    console.log('before modolus hours', hours);
 
     seconds = seconds % 60;
     minutes = minutes % 60;
     hours = hours % 24;
 
-    console.log('after modolus seconds', seconds);
-    console.log('after modolus minutes', minutes);
-    console.log('after modolus hours', hours);
+    timeObj.seconds = Math.floor(seconds);
+    timeObj.minutes = Math.floor(minutes);
+    timeObj.hours = Math.floor(hours);
 
-    if(seconds = 0) {
-      console.log('if seconds');
-      $scope.seconds = '0';
-    } else {
-      console.log('else seconds', seconds);
-      $scope.seconds = Math.floor(seconds);
-    }
-
-    if(minutes = 0) {
-      console.log('if minutes');
-      $scope.minutes = '0';
-    } else {
-      console.log('else minutes', minutes);
-      $scope.minutes = Math.floor(minutes);
-    }
-
-    if(hours < 1) {
-      $scope.hours = Math.floor(hours);
-    }
-
-    $scope.seconds = seconds;
-    $scope.minutes = minutes;
-
-    console.log('$scope.seconds', $scope.seconds);
-    console.log('$scope.minutes', $scope.minutes);
+    return timeObj;
   };
 
   $scope.calculateWorkOut = function() {
     console.log('calc workout func');
   };
 
-  $scope.parseSeconds($scope.roundTimeRemaining);
-  // $scope.parseSeconds($scope.totalTimeRemaining);
+  $scope.roundTimeRemainingObj = $scope.parseSeconds($scope.roundTimeRemaining);
+  $scope.totalTimeRemainingObj = $scope.parseSeconds($scope.totalTimeRemaining);
 })
